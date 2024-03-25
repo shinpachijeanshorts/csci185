73ad7255683ac16434e085ebcc4cdbc8501e88ba
shinpachijeanshorts <edgymiles@gmail.com>
2024-03-25 11:58:11 -0400

    tutorial7

--- lectures/lecture16/02-function-with-parameters/main.js
+++ lectures/lecture16/02-function-with-parameters/main.js
@@ -1,1 +1,13 @@
 // make a function:
+
+function makeRed() {
+    document.querySelector("body").style.backgroundColor = "red";
+}
+
+function makeBlue() {
+    document.querySelector("body").style.backgroundColor = "blue";
+}
+
+function makeGreen() {
+    document.querySelector("body").style.backgroundColor = "green";
+}
